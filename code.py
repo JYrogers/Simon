986740e0aeed03671babd45ebf5ab57232b6c9ad
JYrogers <2025rogers.jacobi@fcboe.org>
2024-10-01 13:15:40 -0400

    Update simon.py

--- code.py
+++ code.py
@@ -3,53 +3,40 @@ import time
 import board
 from digitalio import DigitalInOut, Direction
 
-led = DigitalInOut(board.D2)
-led.direction = Direction.OUTPUT
-led2 = DigitalInOut(board.D3)
-led2.direction = Direction.OUTPUT
-# pin configuration / setup
-white = DigitalInOut(board.D6)
-orange = DigitalInOut(board.D7)
-black = DigitalInOut(board.D8)
-
+white = DigitalInOut(board.D10)
 white.direction = Direction.INPUT
-orange.direction = Direction.INPUT
-black.direction = Direction.INPUT
+bluebtn = DigitalInOut(board.D8)
+bluebtn.direction = Direction.INPUT
+greenbtn = DigitalInOut(board.D6)
+greenbtn.direction = Direction.INPUT
+yellowbtn = DigitalInOut(board.D9)
+yellowbtn.direction = Direction.INPUT
+redbtn = DigitalInOut(board.D7)
+redbtn.direction = Direction.INPUT
+
+red = DigitalInOut(board.D2)
+green = DigitalInOut(board.D4)
+yellow = DigitalInOut(board.D3)
+blue = DigitalInOut(board.D5)
+
+red.direction = Direction.OUTPUT
+green.direction = Direction.OUTPUT
+yellow.direction = Direction.OUTPUT
+blue.direction = Direction.OUTPUT
+
+sequence = []
+score = 0
 
 # main / infinite loop
-while True:
-    if white.value:
-        led.value = not led.value
-        time.sleep(.3)      
-    if orange.value:
-        time.sleep(.3)
-        led2.value = not led2.value
-    if black.value:
-        print("Button with pull-down was pressed")
-        time.sleep(.3)    
-
-#Change the list name for my comfortability 
-list1 = []
-game_seq = []
 
 def add_to_sequence(lsit):
     lsit.append(random.randint(0 , 3))
+    sequence.append(new_color)
+    
+ 
 
-def display_sequence(lsit):
-  #The P was there to see if the buttons were working. 
-    for x in lsit:
-        if x == 0:
-            p = "red"
-            print(p)
-        elif x == 1:
-            p = "green"
-            print(p)
-        elif x == 2:
-            p = "Yellow"
-            print(p)
-        else:
-            p = "Blue"
-            print(p)
+
+def display_sequence():
 
 colors = (1,2,3,4)
 def add(points):
